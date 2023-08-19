e18132e1d9e616b9de3bd08c532f19be898a8bc8
CyreneTsai <cyrenetsai.p@gmail.com>
2023-08-20 00:28:45 +0800

    更新一些老師教的顧客東東

--- FitMatch-BackEnd/Controllers/MemberController.cs
+++ FitMatch-BackEnd/Controllers/MemberController.cs
@@ -33,26 +33,90 @@ namespace FitMatch_BackEnd.Controllers
 
 
         //--------- M-1:會員管理列表（檢視list）=> 前端畫面 index_Backage_member_management.html
+        public IActionResult List(CKeywordViewModel vm)
+        {
 
-
+            FitMatchDbContext db = new FitMatchDbContext();
+            IEnumerable<Member> datas = null;
+            if (string.IsNullOrEmpty(vm.txtKeyword))
+                datas = from p in db.TCustomers
+                        select p;
+            else
+                datas = db.TCustomers.Where(t => t.FName.Contains(vm.txtKeyword)
+                || t.FPhone.Contains(vm.txtKeyword)
+                || t.FPhone.Contains(vm.txtKeyword)
+                || t.FAddress.Contains(vm.txtKeyword));
+            return View(datas);
+        }
 
 
         //--------- M-2:會員新增（Create）
-
-
-
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TCustomer p)
+        {
+            DbDemoContext db = new DbDemoContext();
+            db.TCustomers.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
 
 
 
         //--------- M-3:會員修改（Update）
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("List");
+            DbDemoContext db = new DbDemoContext();
+            TCustomer cust = db.TCustomers.FirstOrDefault(t => t.FId == id);
+            if (cust == null)
+                return RedirectToAction("List");
+            return View(cust);
+        }
+        [HttpPost]
+        public IActionResult Edit(TCustomer custIn)
+        {
+            DbDemoContext db = new DbDemoContext();
+            TCustomer custDb = db.TCustomers.FirstOrDefault(t => t.FId == custIn.FId);
 
-
-
+            if (custDb != null)
+            {
+                custDb.FName = custIn.FName;
+                custDb.FPhone = custIn.FPhone;
+                custDb.FEmail = custIn.FEmail;
+                custDb.FAddress = custIn.FAddress;
+                custDb.FPassword = custIn.FPassword;
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
 
 
 
 
         //--------- M-4:會員刪除（Delete）
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("List");
+            DbDemoContext db = new DbDemoContext();
+            TCustomer cust = db.TCustomers.FirstOrDefault(t => t.FId == id);
+            if (cust != null)
+            {
+                db.TCustomers.Remove(cust);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+
+
+
+
 
     }
 }
