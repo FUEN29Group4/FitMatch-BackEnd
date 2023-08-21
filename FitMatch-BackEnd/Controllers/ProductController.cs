072d9c1bf9cefc83c56e668c791b5b54840b8a08
Air505 <fuen29wei@gmail.com>
2023-08-21 08:40:18 +0800

    products

--- FitMatch-BackEnd/Controllers/ProductController.cs
+++ FitMatch-BackEnd/Controllers/ProductController.cs
@@ -1,24 +1,20 @@
 ﻿using FitMatch_BackEnd.Models;
+using FitMatch_BackEnd.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FitMatch_BackEnd.Controllers
 {
     public class ProductController : Controller
     {
+        //連結DB
         private readonly FitMatchDbContext _context;
 
-        //連結DB
+        
         public ProductController(FitMatchDbContext context)
         {
             _context = context;
         }
-        //GET:ProductType
-        public IActionResult ProductTypeList()
-        {
-            List<Product> datas = (new ProductType()).queryAll();
-                return View(datas);
-        }
-        //商品管理資料連結然後呈現出views
+        
         public IActionResult ProductType()
         {
             IEnumerable<ProductType> datas = from p in _context.ProductTypes select p;
@@ -29,5 +25,24 @@ namespace FitMatch_BackEnd.Controllers
             IEnumerable<Product> datas = from p in _context.Products select p;
             return View(datas);
         }
+
+        //取得產品管理頁面
+        public IActionResult List(CKeywordViewModel vm)
+        {
+            FitMatchDbContext db = new FitMatchDbContext();
+            IEnumerable<Product> datas = null;
+            if (string.IsNullOrEmpty(vm.txtKeyword))
+                datas = from p in db.Products
+                        select p;
+            else
+                datas = db.Products.Where(t => t.ProductName.Contains(vm.txtKeyword));
+            return View(datas);
+        }
+        //public IActionResult ProductTypeList()
+        //{
+        //    List<Product> datas = (new ProductType()).queryAll();
+        //        return View(datas);
+        //}
+        //商品管理資料連結然後呈現出views
     }
 }
