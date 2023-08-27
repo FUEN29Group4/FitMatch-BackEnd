283f5f721a340323f630555a0284c58148c5260a
yvonne <110969170+yvonnehehe@users.noreply.github.com>
2023-08-27 16:07:36 +0800

    未完成驗證

--- FitMatch-BackEnd/Models/Employee.cs
+++ FitMatch-BackEnd/Models/Employee.cs
@@ -9,24 +9,25 @@ public partial class Employee
 {
     public int EmployeeId { get; set; }
 
-    [Required(ErrorMessage = "*")]
-    [MinLength(2, ErrorMessage = "至少2個字")]
-    [RegularExpression(@"^[\u4e00-\u9fa5]+$", ErrorMessage = "請輸入中文")]
+
+    //[Required(ErrorMessage = "*")]
+    //[MinLength(2, ErrorMessage = "至少2個字")]
+    //[RegularExpression(@"^[\u4e00-\u9fa5]+$", ErrorMessage = "請輸入中文")]
     public string? EmployeeName { get; set; }
-    [Required(ErrorMessage = "*")]
+    //[Required(ErrorMessage = "*")]
     public bool? Gender { get; set; }
-    [Required(ErrorMessage = "*")]
+    //[Required(ErrorMessage = "*")]
 
     public DateTime? Birth { get; set; }
-    [Required(ErrorMessage = "*")]
-    [RegularExpression(@"^[0-9-+\s()]*$", ErrorMessage = "請輸入數字")]
+    //[Required(ErrorMessage = "*")]
+    //[RegularExpression(@"^[0-9-+\s()]*$", ErrorMessage = "請輸入數字")]
 
     public string? Phone { get; set; }
-    [Required(ErrorMessage = "*")]
+    //[Required(ErrorMessage = "*")]
 
     public string? Address { get; set; }
-    [Required(ErrorMessage = "*")]
-    [EmailAddress(ErrorMessage = "請輸入正確的Email")]
+    //[Required(ErrorMessage = "*")]
+    //[EmailAddress(ErrorMessage = "請輸入正確的Email")]
 
     public string? Email { get; set; }
 
@@ -36,8 +37,8 @@ public partial class Employee
     public string? Photo { get; set; }
 
     public string? Position { get; set; }
-    [Required(ErrorMessage = "*")]
-    [MinLength(8, ErrorMessage = "至少8個字")]
+    //[Required(ErrorMessage = "*")]
+    //[MinLength(8, ErrorMessage = "至少8個字")]
 
 
     public string? Password { get; set; }
