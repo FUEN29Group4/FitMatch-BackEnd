e07697def1fee6225ee50be66d5819f6a75a3725
20001112 <gno2089922@gmail.com>
2023-10-02 15:04:44 +0800

    20231002 1504

--- FitMatch-BackEnd/ViewModel/OrderViewModel.cs
+++ FitMatch-BackEnd/ViewModel/OrderViewModel.cs
@@ -9,15 +9,15 @@ namespace FitMatch_BackEnd.ViewModel
         public int OrderId { get; set; }
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public DateTime OrderTime { get; set; }
+        public DateTime? OrderTime { get; set; }
         [DisplayName("訂購時間")]
-        public DateTime PayTime { get; set; }
+        public DateTime? PayTime { get; set; }
 
         [DisplayName("付款時間")]
 
-        public int MemberId { get; set; }
+        public int? MemberId { get; set; }
 
-        public string MemberName { get; set; }
+        public string? MemberName { get; set; }
         public int Quantity { get; set; }
 
         [DisplayName("購買數量")]
@@ -37,8 +37,8 @@ namespace FitMatch_BackEnd.ViewModel
         public string? Phone { get; set; }
         public string? Address { get; set; }
         public int? TotalPrice { get; set; }
-        public string ShippingMethod {get ; set; }
-        public string PaymentMethod { get; set;}
+        public string? ShippingMethod {get ; set; }
+        public string? PaymentMethod { get; set;}
 
 
     }
